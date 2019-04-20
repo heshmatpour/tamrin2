844f5f8640543b5ca9ecf17555d6aa210404c861
heshmatpour <49813961+heshmatpour@users.noreply.github.com>
2019-04-21 01:32:57 +0430

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,5 +1,20 @@
 #include "lcd.h"
+#include "stm32f4xx_hal.h"
+
+static GPIO_TypeDef* PORT_LCD;	
+static uint8_t state;		    	
+static uint16_t D[8];	
+static GPIO_TypeDef* port_EN;
+static GPIO_TypeDef* port_RS;
+static uint16_t PIN_RS, PIN_EN;
+
 void lcd_init(lcd_t * lcd)
 {
-	
+	for(i=0 ;i<8 ;i=i+1 )
+	{
+		D[i]= lcd_t.data_pins[i];
+	}
+	PIN_EN=lcd_t.en_port;
+	PIN_RS=lcd_t.rs_port;
+	state=mode;	
 }
