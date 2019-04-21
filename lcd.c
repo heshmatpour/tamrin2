633ede76370d572dc7b651109748463524a15d7d
heshmatpour <49813961+heshmatpour@users.noreply.github.com>
2019-04-21 04:33:35 +0430

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -50,3 +50,18 @@ HAL_GPIO_WritePin(GPIOA,(0x01<<D0_PIN_Start),GPIO_PIN_SET);
 HAL_GPIO_WritePin(GPIOB,(1<<en_pin),GPIO_PIN_SET);
 HAL_GPIO_WritePin(GPIOB,(1<<en_pin),GPIO_PIN_RESET);
 }
+void lcd_set_curser(lcd_t * lcd, uint16_t row, uint16_t col)
+{
+uint16_t maskData;
+maskData = (col-1)&0x0F;
+	 if(row==1)
+	{
+		maskData |= (0x80);
+		 write(lcd ,maskData);
+	 }
+		 	else
+	{
+		maskData |= (0xc0);
+		write(lcd ,maskData);
+			}
+}
